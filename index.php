1457c041d021d3e3802082ede09bcb0826b42839
Dustin Griffith <dustin@dutgriff.com>
2015-01-30 08:58:44 -0600

    Updated front page

--- index.php
+++ index.php
@@ -11,18 +11,17 @@
     you are getting the best experience out of this site and everything else on the
     internet, check out <a href="http://browsehappy.com">BrowseHappy.com</a> and 
     update to the latest version of your preferred browser. BrowseHappy is safe! 
-    It directs you to the developers website to download the browser. This will 
-    ensure that you don't download viruses with the browser. If you are not sure 
-    which browser is best for you, check out Chrome.
+    It directs you to the developer's website to download the browser. This will
+    ensure that you don't download harmful software along with the browser. If you
+    are not sure which browser is best for you, check out Chrome.
   </p>
   <p>  
-    Although this project is not complete, all code should give correct results. If 
-    you come across incorrect results or have any suggestions feel free to 
-    <a href="mailto:dutgriff@dutgriff.com?subject=RocketProject">Email Me</a>.
-    If you are emailing about an error please include as much of the following 
-    information as possible:
+    Although this project is not complete, all code that is not marked as "Dev" should
+    give correct results. If you come across incorrect results or have any suggestions,
+    feel free to <a href="mailto:dutgriff@dutgriff.com?subject=RocketProject">Email Me</a>.
+    If you are emailing about an error please include as much of the following information
+    as possible:
     <ul>
-      <li>Your Operating System</li>
       <li>Your Browser and version</li>
       <li>The data that gives the incorrect results</li>
       <li>What kind of error you are seeing</li>
